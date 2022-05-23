7de30cf6bd429c6ddb51945ae7d1e86655e37ce8
Azkieel <red.ezequiel@gmail.com>
2022-05-23 23:03:24 +0800

    write pseudo code

---
+++ tic-tac-toe.rb
@@ -0,0 +1,26 @@
+# Game rules
+  # Win conditions
+    # Horizontal
+      # 1 - 2 - 3
+      # 4 - 5 - 6
+      # 7 - 8 - 9
+    # Vertical
+      # 1 - 4 - 7
+      # 2 - 5 - 8
+      # 3 - 6 - 9
+    # Diagonal
+      # 1 - 5 - 9
+      # 3 - 5 - 7  
+
+  # Draw conditions
+    # Both players cannot achieve winning conditions 
+    # Both players ran out of moves
+#
+
+# Board creation and purpose
+  # Make board tiles as corresponding numbers from 1 - 9
+  # Record each player moves
+
+# Players 
+  # Can choose which tile number to place their symbol
+  
